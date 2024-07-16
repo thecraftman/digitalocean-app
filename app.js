ddfab45bc0ddcc4421995a4786046be8a75604ec
thecraftman <temitopetola@gmail.com>
2024-07-16 18:32:16 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, DigitalOcean App Platform!');
+});
+
+app.listen(port, () => {
+  console.log(`App is running at http://localhost:${port}`);
+});
